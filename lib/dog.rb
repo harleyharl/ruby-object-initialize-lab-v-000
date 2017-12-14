f9d7a27c2bd43fc3cb55ddad308fdc652237528f
Harley Rayner <harleymteden@gmail.com>
2017-12-14 20:17:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,7 @@
 class Dog
-  # def initialize(name)
-  #   @name = name
-  # end
-  # def initialize(breed)
-  #   @breed = breed
-  # end
+  def initialize(name, breed)
+    @name = name && @breed = breed
+  end
   def name=(name)
     @name = name
   end
